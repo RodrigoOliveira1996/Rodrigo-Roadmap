81717f05c5e3aee9e53221fd2c7274678c9ed98d
Rodrigo Oliveira <rodrigo.n.souza43@gmail.com>
2026-04-24 16:21:10 -0400

    adiciona exercicios em c das aulas do pietro martins

---
+++ linguagem-c/exercicios/pietro_martins/01-formato_printf/formato_printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  printf("oi, tudo bem? tenho 6 anos e programo.\n");
+  printf("Valor inteirp: %d.\n", 10);
+  printf("Valor real: %f.\n", 3.14193);
+  printf("Valor real com apenas duas casas: %.2f.\n", 3.141593);
+  printf("Dado de texto char: %c.\n", 'a');
+  printf("Dado de texto arrays: %s.\n", "testando");
+
+
+
+return 0;
+}
