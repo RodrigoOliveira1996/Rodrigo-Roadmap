ffe6416aa5021f64ef2963b1ba3c60b659499bbb
Rodrigo Oliveira <rodrigo.n.souza43@gmail.com>
2026-04-24 16:59:32 -0400

    adiciona exercicio 3 do pietro martins

---
+++ linguagem-c/exercicios/pietro_martins/02-printf_scape/02-printf_scape.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Imprimir \" \\ \" \n");
+    printf("Imprimir \" (\")  \" \n");
+    printf("Imprimir \" \'  \" \n");
+    printf("Imprimir \" \?  \" \n");
+    printf("Imprimir \" \123  \" \n");
+    printf("Imprimir \" \x12  \" \n");
+
+return 0;}
