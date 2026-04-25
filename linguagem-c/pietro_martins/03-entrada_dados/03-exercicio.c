4173d6ef04203241f63b09bc01019aaf4b4ee6ea
Rodrigo Oliveira <rodrigo.n.souza43@gmail.com>
2026-04-25 10:55:06 -0400

    adiciona exercicio 03 de pietro martins

--- linguagem-c/pietro_martins/03-entrada_dados/03-exercicio.c
+++ linguagem-c/pietro_martins/03-entrada_dados/03-exercicio.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(){
+
+  int idade;
+  printf("Valor inicial idade: %d.\n", idade);
+
+  printf("Digite uma idade: \n");
+  scanf("%d", &idade);
+
+  printf("Idade informada: %d.\n", idade);
+
+return 0;
+}
