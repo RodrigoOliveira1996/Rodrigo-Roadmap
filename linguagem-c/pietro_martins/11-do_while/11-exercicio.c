d21187c977fe916047edd817f042c12f09b6c5d0
Rodrigo Oliveira <rodrigo.n.souza43@gmail.com>
2026-04-27 10:14:29 -0400

    adiciona exercicio 11-do_while

---
+++ linguagem-c/pietro_martins/11-do_while/11-exercicio.c
@@ -0,0 +1,16 @@
+// do executado pelo menos uma vez.
+// //um uso, poderia ser validar um nome de usuario ou alguma informação importante.
+#include <stdio.h>
+
+int main(void){
+
+  int n = 1;
+
+  do{
+    printf("%d\n", n);
+    n++;
+  }
+  while (n <= 10);
+
+return 0;
+}
