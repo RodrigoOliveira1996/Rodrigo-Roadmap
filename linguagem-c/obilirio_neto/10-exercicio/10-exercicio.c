0841e98e0c5f26a4c616c817a6fbb0c3fd60ccae
Rodrigo Oliveira <rodrigo.n.souza43@gmail.com>
2026-05-04 12:01:53 -0400

    adiciona exercicio em C - formataão de strings do professor obilirio neto

--- linguagem-c/obilirio_neto/10-exercicio/10-exercicio.c
+++ linguagem-c/obilirio_neto/10-exercicio/10-exercicio.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
+#include <ctype.h>
 
-
-#define N 50
+#define N 100
 
 int main(void){
 
     char str1[N];
     char str2[N];
-
+    char str3[N];
+    char str4[N];
     printf("Digite um texto para variavel origem: ");
     
-    gets(str1);
-    gets(str2);
+    fgets(str1, N, stdin);
+    str1[strcspn(str1, "\n")] = 0;
 
-    printf()"Tamanhos: %d e %d", strlen(str1), strlen(str2);
-    printf("Minusculas: %s")
+    printf("Digite um texto para variavel destino: ");
+    
+    fgets(str2, N, stdin);
+    str2[strcspn(str2, "\n")] = 0;
 
-    strcpy(destino, origem);
-    printf("Texto origem: %s\nTexto destino: %s\n", origem, destino);
+    printf("Tamanhos: %zu e %zu \n", strlen(str1), strlen(str2));
+
+    printf("Minusculas: \n");
+
+    for(int i = 0; str1[i]; i++){
+      putchar(tolower(str1[i]));
+    }
+    printf("\n");
+  
+    printf("Maiúsculas: \n");
+    for(int i = 0; str2[i]; i++){
+      putchar(toupper(str2[i]));
+    
+    }
+
+    printf("\n");
+
+    printf("ordem alfabetica: \n");
+
+    if(strcmp(str1, str2) <0)
+      printf("%s ||| %s\n", str1, str2);
+    
+    else
+      printf("%s ||| %s\n", str2, str1);
+
+    strcpy(str3, str1);
+    printf("concatenação: \n");
+    printf("%s\n",  strcat(str3, str2));
+
+    strcpy(str2, str1);
+    printf("Texto origem: %s\nTexto destino: %s\n", str1, str2);
 
 return 0;
 }
